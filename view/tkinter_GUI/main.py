c8e10453475908362a7fb13cdb1a45386ae40c81
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-07-21 22:35:32 +0200

    Create a View Frame which displays information about what is Enterprise Resource Planning. Display the Frame in main app's window.

--- view/tkinter_GUI/main.py
+++ view/tkinter_GUI/main.py
@@ -1,9 +1,10 @@
 from tkinter import *
-
+from view.tkinter_GUI import about_pages
 
 app = Tk()
 app.title('Asparagusz - Secure Enterprise Resource Planning Software')
-app.geometry("1300x650")
+app.geometry("1300x700")
 app.iconbitmap('./view/tkinter_GUI/assets/asparagusz.ico')
+about_pages.display_about_enterprise_resource_planning(app)
 
 app.mainloop()
