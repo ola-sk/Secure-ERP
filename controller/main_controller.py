79009a3f4f7b6a703e0f8526d978eca0766b389a
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-07-30 10:25:16 +0200

    Change the return of the run_the_app() function in main_controller.py. There is yet no use of returning the `app` from that function.

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -8,16 +8,13 @@ from view import terminal as view
 # from view.tkinter_GUI import main as view
 
 
-def run_the_app() -> Tk or None:
+def run_the_app() -> None:
 	app: Tk or None = view.initialise()
 	# TODO: instead displaying `about ERP` display the log-in form upon successful log-in show main menu
-	if app is None:
-		main_menu()
-	elif isinstance(app, Tk):
-		view.display_about_enterprise_resource_planning(app)
+	main_menu(app)
 	view.finalise(app)
 
-	return app
+	return None
 
 
 def load_module(option) -> None:
