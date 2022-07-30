ed2271144d15dd0a5e9eec27552d6166a0cb88c3
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-07-30 12:47:35 +0200

    Revive the version of hr_controller.py that worked in integration with the rest of the program (uncomment the previous content).

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -5,8 +5,10 @@ from view import terminal as view
 
 from email import message
 
+
 def add_employee():
 	view.print_error_message("Not implemented yet.")
+
 
 def update_employee():
 	view.print_error_message("Not implemented yet.")
@@ -20,69 +22,69 @@ def get_oldest_and_youngest():
 	view.print_error_message("Not implemented yet.")
 
 
-# def get_average_age():
-# 	view.print_error_message("Not implemented yet.")
+def get_average_age():
+	view.print_error_message("Not implemented yet.")
 
 
-# def next_birthdays():
-# 	view.print_error_message("Not implemented yet.")
+def next_birthdays():
+	view.print_error_message("Not implemented yet.")
 
 
-# def count_employees_with_clearance():
-# 	view.print_error_message("Not implemented yet.")
+def count_employees_with_clearance():
+	view.print_error_message("Not implemented yet.")
 
 
-# def count_employees_per_department():
-# 	view.print_error_message("Not implemented yet.")
+def count_employees_per_department():
+	view.print_error_message("Not implemented yet.")
 
 
-# def run_operation(option):
-# 	if option == 1:
-# 		main_hr()
-# 	elif option == 2:
-# 		add_employee()
-# 	elif option == 3:
-# 		update_employee()
-# 	elif option == 4:
-# 		delete_employee()
-# 	elif option == 5:
-# 		get_oldest_and_youngest()
-# 	elif option == 6:
-# 		get_average_age()
-# 	elif option == 7:
-# 		next_birthdays()
-# 	elif option == 8:
-# 		count_employees_with_clearance()
-# 	elif option == 9:
-# 		count_employees_per_department()
-# 	elif option == 0:
-# 		return
-# 	else:
-# 		raise KeyError("There is no such option.")
+def run_operation(option):
+	if option == 1:
+		main_hr()
+	elif option == 2:
+		add_employee()
+	elif option == 3:
+		update_employee()
+	elif option == 4:
+		delete_employee()
+	elif option == 5:
+		get_oldest_and_youngest()
+	elif option == 6:
+		get_average_age()
+	elif option == 7:
+		next_birthdays()
+	elif option == 8:
+		count_employees_with_clearance()
+	elif option == 9:
+		count_employees_per_department()
+	elif option == 0:
+		return
+	else:
+		raise KeyError("There is no such option.")
 
 
-# def display_menu():
-# 	options = [
-# 		"Back to main menu",
-# 		"List employees",
-# 		"Add new employee",
-# 		"Update employee",
-# 		"Remove employee",
-# 		"Oldest and youngest employees",
-# 		"Employees average age",
-# 		"Employees with birthdays in the next two weeks",
-# 		"Employees with clearance level",
-# 		"Employee numbers by department"]
-# 	view.print_menu("Human resources", options)
+def display_menu():
+	options = [
+		"Back to main menu",
+		"List employees",
+		"Add new employee",
+		"Update employee",
+		"Remove employee",
+		"Oldest and youngest employees",
+		"Employees average age",
+		"Employees with birthdays in the next two weeks",
+		"Employees with clearance level",
+		"Employee numbers by department"]
+	view.print_menu("Human resources", options)
 
 
-# def menu():
-# 	operation = None
-# 	while operation != '0':
-# 		display_menu()
-# 		try:
-# 			operation = view.get_input("Select an operation")
-# 			run_operation(int(operation))
-# 			helpers.get_user_acknowledgement("<< Press Enter >>")
-# 		except KeyError as error:
-# 			view.print_error_message(str(error))
+def menu():
+	operation = None
+	while operation != '0':
+		display_menu()
+		try:
+			operation = view.get_input("Select an operation")
+			run_operation(int(operation))
+			helpers.get_user_acknowledgement("<< Press Enter >>")
+		except KeyError as error:
+			view.print_error_message(str(error))
