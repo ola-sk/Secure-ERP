320b9a1b5fae9b165b38be195c73381f55e05142
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-07-30 11:55:56 +0200

    Add docstring to initialise() in main.py in tkinter GUI view.

--- view/tkinter_GUI/main.py
+++ view/tkinter_GUI/main.py
@@ -7,6 +7,9 @@ from view.tkinter_GUI.modules.sales import *
 
 
 def initialise() -> Tk:
+	"""Initialises graphical elements of User Interface. Creates an instance of a Tkinter application named `app` and
+	sets its parameters, such as title, size of the window, icon. In this function also Images that are going to be used
+	throughout the application, like a button image, are initialised to be available in the application."""
 	app = Tk()
 	app.title('Asparagusz - Secure Enterprise Resource Planning Software')
 	app.geometry("1300x700")
